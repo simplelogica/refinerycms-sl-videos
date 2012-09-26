5f7b0a34648c22ca8125f0630b63715dfe210171
Ruben Sierra <ruben.sierra@the-cocktail.com>
2012-09-26 18:00:22 +0200

    Attribute accessor for video data, loaded from unbolt gem on initialization.

--- app/models/refinery/videos/video.rb
+++ app/models/refinery/videos/video.rb
@@ -3,6 +3,8 @@ module Refinery
     class Video < Refinery::Core::BaseModel
       self.table_name = 'refinery_videos'
 
+      attr_accessor :video_data
+
       translates :title, :video_text, :video_url, :custom_url, :slug
 
       extend FriendlyId
@@ -15,8 +17,17 @@ module Refinery
       validates :title, :presence => true, :uniqueness => true
       validates :video_url, :presence => true
 
+      after_initialize :initialize_attributes
       def friendly_id_source
         custom_url.presence || title
+      end
+
+      def initialize_attributes
+        self.video_data = self.load_video_data
+      end
+
+      def load_video_data
+        self.video_url.blank? ? nil : (UnvlogIt.new(self.video_url) rescue nil)
       end
     end
   end
